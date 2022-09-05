81531957790940e3f77c7592f82c1e5483aec97f
Mariano Goldman <mariano.goldman@decentraland.org>
2022-09-05 09:32:27 -0300

    generate both screenshots at once

--- src/controllers/handlers/screenshot-handler.ts
+++ src/controllers/handlers/screenshot-handler.ts
@@ -1,22 +1,29 @@
-import { HandlerContextWithPath, Network, Type } from "../../types";
-import { IHttpServerComponent } from "@well-known-components/interfaces";
-import { generateScreenshot } from "../../logic/generate-screenshot";
-import IResponse = IHttpServerComponent.IResponse;
+import { HandlerContextWithPath, Network, Type } from "../../types"
+import { IHttpServerComponent } from "@well-known-components/interfaces"
+import { generateScreenshots } from "../../logic/generate-screenshot"
+import * as Buffer from "buffer"
+import * as fs from "fs"
+import IResponse = IHttpServerComponent.IResponse
 
 export function createScreenshotHandler(network: Network, type: Type) {
-  return async (context: Pick<HandlerContextWithPath<"metrics" | "config" | "fetch" | "browser", "/mainnet/face/:address/:hash">, "url" | "components" | "params">): Promise<IResponse> => {
-    const {address, hash} = context.params
+  return async (
+    context: Pick<
+      HandlerContextWithPath<"metrics" | "config" | "fetch" | "browser", "/mainnet/face/:address/:hash">,
+      "url" | "components" | "params"
+    >
+  ): Promise<IResponse> => {
+    const { address, hash } = context.params
     if (!address) {
       return {
         status: 400,
-        body: {error: 'Invalid address'}
+        body: { error: "Invalid address" },
       }
     }
 
     if (!hash) {
       return {
         status: 400,
-        body: {error: 'Invalid hash'}
+        body: { error: "Invalid hash" },
       }
     }
 
@@ -29,15 +36,28 @@ export function createScreenshotHandler(network: Network, type: Type) {
       pathname: url.pathname,
     })
 
-    const screenshot = await generateScreenshot(context.components, network, type, address)
+    const screenshot = await generateScreenshots(context.components, network, address)
+
+    // @ts-ignore
+    writeFileSync("mariano-body.png", screenshot.body)
+    writeFileSync("mariano-face.png", screenshot.face)
+    // end of 2nd shot
 
     return {
       status: 200,
       headers: {
-        "content-type": 'image/png',
-        "cache-control": 'public, max-age=2592000, s-maxage=2592000' // 30 days of cache
+        "content-type": "image/png",
+        "cache-control": "public, max-age=2592000, s-maxage=2592000", // 30 days of cache
       },
-      body: screenshot
+      body: screenshot.body,
     }
+  }
+}
+
+const writeFileSync = function (path: string, buffer: Buffer.Buffer) {
+  const fileDescriptor = fs.openSync(path, "w")
+  if (fileDescriptor) {
+    fs.writeSync(fileDescriptor, buffer, 0, buffer.length, 0)
+    fs.closeSync(fileDescriptor)
   }
 }
--- src/logic/generate-screenshot.ts
+++ src/logic/generate-screenshot.ts
@@ -1,6 +1,7 @@
-import {AppComponents, Network, Type} from "../types";
-import { Browser } from "puppeteer";
-import {IConfigComponent} from "@well-known-components/interfaces";
+import { AppComponents, Network, Type } from "../types"
+import { Page } from "puppeteer"
+import { IConfigComponent } from "@well-known-components/interfaces"
+import * as Buffer from "buffer"
 
 export type ViewPort = {
   width: number
@@ -13,92 +14,109 @@ export type Clip = {
   width: number
   height: number
 }
+export const generateScreenshots = async (
+  components: Pick<AppComponents, "browser" | "config">,
+  network: Network,
+  address: string
+): Promise<Screenshots> => {
+  const page = await components.browser.newPage()
+  await page.setUserAgent(
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36"
+  )
 
-export const generateScreenshot = async (components: Pick<AppComponents, 'browser' | 'config'>, network: Network, type: Type, address: string) => {
-  const fetchUrl = await getUrl(components.config, network, type, address)
-  const viewport = getViewPort(type)
-  const clip = getClip(type)
-
-  return await getScreenshot(components.browser, fetchUrl, viewport, clip)
-}
-
-const getScreenshot = async (browser: Browser, url: string, viewport: ViewPort, clip?: Clip) => {
-  const timer = createTimer()
-
-  const page = await browser.newPage()
-
-  // attach cdp session to page
-  const client = await page.target().createCDPSession();
-  await client.send('Debugger.enable');
-  await client.send('Debugger.setAsyncCallStackDepth', { maxDepth: 32 });
-
-  function limit (string = '', limit = 0) {
-    return string.substring(0, limit)
-  }
-
-  // enable network
-  await client.send('Network.enable');
-  // attach callback to network response event
-    await client.on('Network.responseReceived', (params) => {
-    const { type, response: { url, status, fromDiskCache } } = params;
-    /*
-     * See: https://chromedevtools.github.io/devtools-protocol
-     * /tot/Network/#type-ResourceTiming for complete list of
-     * timing data available under 'timing'
-     */
-    console.log({
-      type, url: limit(url, 100), status, fromDiskCache
-    })
-  });
+  // // attach cdp session to page
+  // const client = await page.target().createCDPSession();
+  // await client.send('Debugger.enable');
+  // await client.send('Debugger.setAsyncCallStackDepth', {maxDepth: 32});
+  //
+  // function limit(string = '', limit = 0) {
+  //   return string.substring(0, limit)
+  // }
+  //
+  // // enable network
+  // await client.send('Network.enable');
+  // // attach callback to network response event
+  // await client.on('Network.responseReceived', (params) => {
+  //   const {type, response: {url, status, fromDiskCache}} = params;
+  //   /*
+  //    * See: https://chromedevtools.github.io/devtools-protocol
+  //    * /tot/Network/#type-ResourceTiming for complete list of
+  //    * timing data available under 'timing'
+  //    */
+  //   console.log({
+  //     type, url: limit(url, 100), status, fromDiskCache
+  //   })
+  // });
 
   try {
-    await page.setViewport({
-      deviceScaleFactor: 2,
-      ...viewport,
-    })
-    timer.openBrowser()
-
-    await page.goto(url)
-    timer.goToUrl()
-
-    const container = await page.waitForSelector('.is-loaded', { timeout: 30_000 })
-    timer.waitForReady()
-
-    if (!container) {
-      throw new Error("Timeout waiting for profile to render.")
+    const body = await capture(page, components.config, network, Type.BODY, address)
+    const face = await capture(page, components.config, network, Type.FACE, address)
+    return {
+      body,
+      face,
     }
-
-    const buffer = await container.screenshot({
-      encoding: 'binary',
-      clip,
-    })
-    timer.takeScreenshot()
-
-    let performance = JSON.parse(await page.evaluate(() => JSON.stringify(performance.getEntries(), undefined, 2)));
-    console.log(performance.map((p: any) => ({type: p.entryType, name: p.name, duration:p.duration})))
-    console.log(timer.timings())
-
-    return buffer
   } finally {
     await page.close()
   }
 }
 
+async function capture(
+  page: Page,
+  config: IConfigComponent,
+  network: Network,
+  type: Type,
+  address: string
+): Promise<Buffer.Buffer> {
+  const url = await getUrl(config, network, type, address)
+  const viewport = getViewPort(type)
+  const clip = getClip(type)
+
+  const timer = createTimer()
+  await page.setViewport({
+    deviceScaleFactor: 2,
+    ...viewport,
+  })
+  timer.openBrowser()
+
+  await page.goto(url)
+  timer.goToUrl()
+
+  const container = await page.waitForSelector(".is-loaded", { timeout: 30_000 })
+  timer.waitForReady()
+
+  if (!container) {
+    throw new Error("Timeout waiting for profile to render.")
+  }
+
+  const buffer = await container.screenshot({
+    encoding: "binary",
+    clip,
+  })
+  timer.takeScreenshot()
+
+  // let performance = JSON.parse(await page.evaluate(() => JSON.stringify(performance.getEntries(), undefined, 2)));
+  // console.log(performance.map((p: any) => ({type: p.entryType, name: p.name, duration: p.duration})))
+  console.log(timer.timings())
+
+  // @ts-ignore
+  return buffer
+}
+
 const getUrl = async (config: IConfigComponent, network: Network, type: Type, address: string): Promise<string> => {
-  const baseUrl = await config.getString("WEARABLES_PREVIEW_URL") || "https://wearable-preview.decentraland.org"
+  const baseUrl = (await config.getString("WEARABLES_PREVIEW_URL")) || "https://wearable-preview.decentraland.org"
   const url = new URL(baseUrl)
-  url.searchParams.append('profile', address)
-  url.searchParams.append('disableBackground', '')
-  url.searchParams.append('autoRotateSpeed', '0')
+  url.searchParams.append("profile", address)
+  url.searchParams.append("disableBackground", "")
+  url.searchParams.append("autoRotateSpeed", "0")
 
   if (type === Type.FACE) {
-    url.searchParams.append('zoom', '70')
-    url.searchParams.append('offsetY', '1.3')
-    url.searchParams.append('centerBoundingBox', 'false')
+    url.searchParams.append("zoom", "70")
+    url.searchParams.append("offsetY", "1.3")
+    url.searchParams.append("centerBoundingBox", "false")
   }
 
   if (network === Network.GOERLI) {
-    url.searchParams.append('env', 'dev')
+    url.searchParams.append("env", "dev")
   }
 
   return url.toString()
@@ -122,11 +140,11 @@ const getClip = (type: Type): Clip | undefined => {
 }
 
 type Timings = {
-  openBrowser: number;
-  goToUrl: number;
-  waitForReady: number;
-  takeScreenshot: number;
-  total: number;
+  openBrowser: number
+  goToUrl: number
+  waitForReady: number
+  takeScreenshot: number
+  total: number
 }
 
 type Timer = {
@@ -137,27 +155,32 @@ type Timer = {
   timings: () => Timings
 }
 
+export type Screenshots = {
+  body: Buffer.Buffer
+  face: Buffer.Buffer
+}
+
 const createTimer = (): Timer => {
-  const startMs = (new Date()).getTime()
+  const startMs = new Date().getTime()
   let openBrowserMs = 0
   let goToUrlMs = 0
   let waitForReadyMs = 0
   let takeScreenshotMs = 0
 
   const openBrowser = () => {
-    openBrowserMs = (new Date()).getTime()
+    openBrowserMs = new Date().getTime()
   }
 
   const goToUrl = () => {
-    goToUrlMs = (new Date()).getTime()
+    goToUrlMs = new Date().getTime()
   }
 
   const waitForReady = () => {
-    waitForReadyMs = (new Date()).getTime()
+    waitForReadyMs = new Date().getTime()
   }
 
   const takeScreenshot = () => {
-    takeScreenshotMs = (new Date()).getTime()
+    takeScreenshotMs = new Date().getTime()
   }
 
   const timings = () => {
@@ -166,7 +189,7 @@ const createTimer = (): Timer => {
       goToUrl: goToUrlMs - openBrowserMs,
       waitForReady: waitForReadyMs - openBrowserMs,
       takeScreenshot: takeScreenshotMs - waitForReadyMs,
-      total: takeScreenshotMs - startMs
+      total: takeScreenshotMs - startMs,
     }
   }
 
